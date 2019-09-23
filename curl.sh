1f8f504708c39a91f85685f5ef25dcd5a80a9429
aCampello <a.campello@wellcome.ac.uk>
2019-09-23 16:01:11 +0100

    Add curl.sh

---
+++ curl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source $HOME/.bash_profile
+
+curl --request POST \
+  --url http://127.0.0.1:8000/random-art/slack \
+  --header 'content-type: application/json' \
+  --data '{
+	"link":"'"$SLACK_HOOK"'"
+}'
