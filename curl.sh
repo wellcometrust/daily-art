6a48bf3db72a25277dfd8bd56dc8e605d6a00397
aCampello <a.campello@wellcome.ac.uk>
2019-09-24 13:02:03 +0100

    Replace SLACK_HOOK directly by env-variable on docker-compose runtime

--- curl.sh
+++ curl.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-SLACK_HOOK=$SLACK_HOOK
-
 curl --request POST \
   --url http://127.0.0.1:8000/random-art/slack \
   --header 'content-type: application/json' \
   --data '{
-	"link":"'"$SLACK_HOOK"'"
+	"link": "SLACK_HOOK"
 }'
--- prestart.sh
+++ prestart.sh
@@ -1,8 +1,9 @@
-#! /usr/bin/env bash
+#!/bin/bash
 
 python daily-art/resources/data.py
 
-if [[ "$SLACK_HOOK" != "" ]]; then
-    cat /app/cronspecs.txt | crontab
+if [ "$SLACK_HOOK" != "" ]; then
+    sed -i /app/curl.sh -e "s@SLACK_HOOK@$SLACK_HOOK@g" /app/curl.sh
+    cat /app/cronspecs.txt | crontab -
     cron
 fi
