3b5b7181bf4d39a8a491fc5d565f747810b25428
aCampello <a.campello@wellcome.ac.uk>
2021-05-08 10:41:49 +0100

    Update similarity

--- daily-art/resources/similarity.py
+++ daily-art/resources/similarity.py
@@ -3,11 +3,11 @@ import requests
 
 def get_visually_similar_artworks(work_id: str, n: int = 3):
     """ Returns a list of `n` visually similar artworks """
-    url = f"https://labs.wellcomecollection.org/feature-similarity/works/{work_id}?n={n}"
+    url = f"https://api.wellcomecollection.org/catalogue/v2/images/{work_id}?include=visuallySimilar"
     similarity_req = requests.get(url)
 
     if similarity_req:
-        return [similarity_api_result_to_similar_work(n) for n in similarity_req.json()["neighbours"]]
+        return [similarity_api_result_to_similar_work(n) for n in similarity_req.json()["visuallySimilar"]]
     else:
         return []
 
@@ -22,11 +22,15 @@ def get_title(work_id: str):
         return ""
 
 
+def convert_iiif_info_to_image_uri(uri):
+    return uri.replace('info.json', '') + 'full/max/0/default.jpg'
+
+
 def similarity_api_result_to_similar_work(neighbour):
     """ Converts results from the similarity API to the model used in daily-art """
     return {
-        "id": neighbour["catalogue_id"],
-        "title": get_title(neighbour["catalogue_id"]),
-        "full_image_uri": neighbour["miro_uri"],
-        "collection_url": neighbour["catalogue_uri"]
+        "id": neighbour['source']['id'],
+        "title": neighbour['source']['title'],
+        "full_image_uri": convert_iiif_info_to_image_uri(neighbour['thumbnail']['url']),
+        "collection_url": f"https://wellcomecollection.org/works/{neighbour['source']['id']}"
     }
