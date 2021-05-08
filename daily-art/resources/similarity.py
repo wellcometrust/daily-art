fdff74df83b84ad863702d94358f66f7ccce807c
aCampello <a.campello@wellcome.ac.uk>
2021-05-08 11:59:06 +0100

    Fix similarity request

--- daily-art/resources/similarity.py
+++ daily-art/resources/similarity.py
@@ -1,15 +1,23 @@
 import requests
 
 
-def get_visually_similar_artworks(work_id: str, n: int = 3):
+def get_visually_similar_artworks(work_id: str, n_works: int = 3):
     """ Returns a list of `n` visually similar artworks """
-    url = f"https://api.wellcomecollection.org/catalogue/v2/images/{work_id}?include=visuallySimilar"
-    similarity_req = requests.get(url)
+    work_url = f"https://api.wellcomecollection.org/catalogue/v2/works/{work_id}?include=images"
 
-    if similarity_req:
-        return [similarity_api_result_to_similar_work(n) for n in similarity_req.json()["visuallySimilar"]]
-    else:
-        return []
+    work_req = requests.get(work_url)
+
+    if work_req and work_req.json().get("images"):
+        sim_url = f"https://api.wellcomecollection.org/catalogue/v2/images/" \
+                  f"{work_req.json()['images'][0]['id']}?include=visuallySimilar"
+
+        similarity_req = requests.get(sim_url)
+
+        if similarity_req:
+            return [similarity_api_result_to_similar_work(n) for n in
+                    similarity_req.json()["visuallySimilar"]][:n_works]
+
+    return []
 
 
 def get_title(work_id: str):
@@ -22,7 +30,7 @@ def get_title(work_id: str):
         return ""
 
 
-def convert_iiif_info_to_image_uri(uri):
+def convert_iiif_info_to_image_uri(uri: str):
     return uri.replace('info.json', '') + 'full/max/0/default.jpg'
 
 
