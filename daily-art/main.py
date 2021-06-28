5748c74f805b6e29b50ee40ef7dade5975c8331d
aCampello <a.campello@wellcome.ac.uk>
2021-06-28 10:58:27 +0100

    :Lipstick:

--- daily-art/main.py
+++ daily-art/main.py
@@ -11,22 +11,21 @@ from starlette.templating import Jinja2Templates
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
 
-from .resources.data import ArtWork, get_data, update_data, convert_iiif_width
+from .resources.data import ArtWork, get_data, save_data_locally, convert_iiif_width
 from .resources.similarity import get_visually_similar_artworks
 from .resources.slack_hook import SlackHook
 
 CHANNEL_ID = 'daily-art-beta'
 
-app = FastAPI(title="Random Wellcome Art", host="0.0.0.0", port=8000)
+app = FastAPI(title="Random Wellcome Art", host="0.0.0.0", port=8000,
+              version='1.0.0')
 app.mount("/static", StaticFiles(directory="daily-art/static"),
           name="static")
 templates = Jinja2Templates(directory="daily-art/templates")
 
 filtered_works = get_data(
-    exclude_sensitive=True, only_interesting=True, exclude_used=False
+    exclude_sensitive=True, only_interesting=True, exclude_used=True
 )
-
-logger.setLevel(logging.INFO)
 
 
 def get_random_artwork(width):
@@ -44,7 +43,9 @@ def get_random_artwork(width):
     )
     non_used_works[work_id]["similar_works"] = get_visually_similar_artworks(work_id)
 
+    # My poor way of persist information without having a database :P
     filtered_works[work_id]["used"] = True
+    save_data_locally(filtered_works)
 
     return non_used_works[work_id]
 
@@ -88,8 +89,16 @@ def random_art_slack(width: int = 600, work_id: str = ""):
         return slack_request
 
 
+from pydantic import Field
+
+
+class WorkId(str):
+    pass
+
+from fastapi import Path
+
 @app.get("/flag/{work_id}")
-def flag_art(work_id: str, sensitivity: bool = False,
+def flag_art(work_id: str = Path(..., description="The id of your work"), sensitivity: bool = False,
              interesting: bool = False,
              redirect: bool = False):
     """ Flags art as sensitive or not """
